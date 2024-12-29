44f0e320bfc291dbb61d3396cb7c0aec7080332c
piotrekkr <piotrekkr@users.noreply.github.com>
2024-12-29 09:45:26 +0000

    Cleanup after cloning from template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+
 
 /**
  * The main function for the action.
@@ -7,18 +7,9 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const dockerfile = core.getMultilineInput('dockerfile', { required: true })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`Checking for base image updates in: ${dockerfile}`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
