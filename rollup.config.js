1604692f33ed17a4ff6b864d2fb0e88801afab41
piotrekkr <piotrekkr@users.noreply.github.com>
2025-11-04 22:31:29 +0100

    Pull new code from actions/javascript-action

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+// See: https://rollupjs.org/introduction/
+
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+const config = {
+  input: 'src/index.js',
+  makeAbsoluteExternalsRelative: true,
+  output: {
+    esModule: true,
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [commonjs(), nodeResolve({ preferBuiltins: true })]
+}
+
+export default config
